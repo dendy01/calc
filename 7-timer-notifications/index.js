c63867e4b3f9c2889e8b311f9ecd1166f1b1e760
dendy01 <biomannumberone@gmail.com>
2026-01-11 02:02:34 +0300

    feature: 7-timer

---
+++ 7-timer-notifications/index.js
@@ -0,0 +1,19 @@
+const { getTimeInMilliseconds } = require("./utils/timer.js");
+const notifier = require("node-notifier");
+const path = require("path");
+
+
+if (process.argv.length <= 2) {
+    console.log("Введите корректное время. Например: 1h 32m 12s");
+} else {
+    const time = process.argv.slice(2);
+
+    setTimeout(() => {
+        console.log("Подъём!!!");
+
+        notifier.notify({
+            title: "Подъём!!!",
+            message: "Поднимайся моя красавица!!!"
+        });
+    }, getTimeInMilliseconds(time));
+}
